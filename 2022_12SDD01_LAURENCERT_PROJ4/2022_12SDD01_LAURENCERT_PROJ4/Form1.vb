049354908f5b6537ed8899869002a878fdf03e8f
FerrulFox <84551430+FerrulFox@users.noreply.github.com>
2022-03-09 18:28:02 +1100

    Add files via upload

---
+++ 2022_12SDD01_LAURENCERT_PROJ4/2022_12SDD01_LAURENCERT_PROJ4/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'i am unable to do this, as i do not posess 64-bit office to import the data file.
+    End Sub
+End Class
